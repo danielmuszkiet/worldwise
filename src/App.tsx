01872cdc47867e8b47af0c314eb922bf4429864e
Daniel Muszkiet <danielmuszkiet.marketing@gmail.com>
2025-06-01 14:25:19 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
+          <Route path="app" element={<AppLayout />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;

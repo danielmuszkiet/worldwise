19f1de61f44eb5081e95c1d277c76a6fa2915025
Daniel Muszkiet <danielmuszkiet.marketing@gmail.com>
2025-06-06 15:56:24 +0200

    update removeCity method signature to return a Promise

--- src/contexts/CitiesContext.ts
+++ src/contexts/CitiesContext.ts
@@ -7,7 +7,7 @@ export type CitiesContextType = {
   isLoading: boolean;
   getCity: (id: string) => void;
   createCity: (newCity: TCity) => Promise<void>;
-  removeCity: (id: string) => void;
+  removeCity: (id: string) => Promise<void>;
 };
 
 export const CitiesContext = createContext<CitiesContextType | null>(null);
